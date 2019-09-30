a6f6e315ff71c6b64dd1a95ae794350c6ac1778a
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-09-30 19:50:43 -0300

    Learning promises

--- AJAX/master.js
+++ AJAX/master.js
@@ -1,3 +1,5 @@
+//Learning Assync AJAX
+/*
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://api.github.com/users/romuloschiavon');
@@ -8,3 +10,33 @@ xhr.onreadystatechange = function(){
         console.log(JSON.parse(xhr.responseText));
     }
 }
+*/
+
+
+//Learning Promises
+var myPromise = function(){
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://api.123123github.com/users/romuloschiavon');
+        xhr.send(null);
+        
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4){
+                if (xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject("Error!!")
+                };
+            };
+        };
+    });
+};
+
+myPromise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
